4b339516a5b96165349a46cab34eeddcf3fdd2bd
DevOsamaIslam <osamasamarrai@gmail.com>
2021-09-26 13:02:18 +0300

    bug fix

--- stats.js
+++ stats.js
@@ -18,7 +18,7 @@ const getWinrate = async () => {
   }).sort('-1').lean()
   let wonOrders = orders.filter(order => order.status === 'TP')
   let lostOrders = orders.filter(order => order.status === 'SL')
-  let winRatio = calcPerc(wonOrders.length, orders.length).toFixed(1)
+  let winRatio = ((wonOrders.length * 100) / orders.length).toFixed(1)
 
   console.log(`No. of trades: ${orders.length}`);
   console.log(`Won: ${wonOrders.length}`);
