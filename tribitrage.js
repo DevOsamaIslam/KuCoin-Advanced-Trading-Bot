50df207caddbf59ec41af974488a54fa06691f18
DevOsamaIslam <osamasamarrai@gmail.com>
2021-10-27 10:36:39 +0300

    bug fix

--- tribitrage.js
+++ tribitrage.js
@@ -61,8 +61,7 @@ const arbitrage = async options => {
     // check if there is an arbitrage opportunity
     // if the output is at least 0.03 bigger than the risked amount
     // and there's no active trade going on
-    if (ownInitial - 0.03 > risked || !x) {
-      x = true
+    if (ownInitial - 0.03 > risked) {
       let coin = getBase(symbols.BC)
       if (!isExcluded(coin)) exclude(coin)
       log(`Arbitrage opportunity`);
