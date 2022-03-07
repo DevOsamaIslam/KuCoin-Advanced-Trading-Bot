9fed3ac74229f83805524bdde1458b09179d0f55
Osama Islam <osamasamarrai@gmail.com>
2022-03-07 14:50:29 +0200

    disable logging

--- log.js
+++ log.js
@@ -3,7 +3,9 @@ import {
 } from 'fs'
 import moment from 'moment'
 
-export default async data => appendFileSync(`./records/logs/${moment().format('YYYY-MM-DD')}.log`, `${new Date()} ${data}\n`)
+export default async data => {
+  // appendFileSync(`./records/logs/${moment().format('YYYY-MM-DD')}.log`, `${new Date()} ${data}\n`)
+}
 
 export const verbose = async data => appendFileSync(`./records/logs/verbose_${moment().format('YYYY-MM-DD')}.log`, `${new Date()} ${data}\n`)
 
