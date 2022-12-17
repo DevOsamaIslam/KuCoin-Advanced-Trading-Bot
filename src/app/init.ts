322fe8f5da6caaad7c9ce5c7b9b41ec0d2c0e71b
Osama <Osamah.a@mblhightech.com>
2022-12-17 18:24:53 +0200

    adding settings and fix initializer

--- src/app/init.ts
+++ src/app/init.ts
@@ -1,8 +1,26 @@
-import cctx from "ccxt"
-import { API_KEY, API_PASSPHRASE, API_SECRET } from "./settings"
+// @ts-ignore
+import SDK from 'kucoin-node-sdk'
+import { IDatafeedObject } from 'lib/types/datafeed'
+import { API_KEY, API_PASSPHRASE, API_SECRET, BASE_URL } from './settings'
 
-export const kucoin = new cctx.kucoin({
-  apiKey: API_KEY,
-  secret: API_SECRET,
-  password: API_PASSPHRASE,
-})
+let datafeed: IDatafeedObject
+
+export const initialize = () => {
+  SDK.init({
+    baseUrl: BASE_URL,
+    apiAuth: {
+      key: API_KEY, // KC-API-KEY
+      secret: API_SECRET, // API-Secret
+      passphrase: API_PASSPHRASE, // KC-API-PASSPHRASE
+    },
+    authVersion: 2, // KC-API-KEY-VERSION. Notice: for v2 API-KEY, not required for v1 version.
+  })
+}
+
+export const getDatafeed = (): IDatafeedObject => {
+  if (datafeed) return datafeed
+
+  datafeed = new SDK.websocket.Datafeed(true)
+  datafeed.connectSocket()
+  return datafeed
+}
--- src/app/settings.ts
+++ src/app/settings.ts
@@ -1,7 +1,9 @@
-import dotenv from "dotenv"
+import dotenv from 'dotenv'
 dotenv.config()
 
-export const BASE_URL = process.env.BASE_URL
-export const API_KEY = process.env.API_KEY
-export const API_SECRET = process.env.SECRET
-export const API_PASSPHRASE = process.env.PASSPHRASE
+export const BASE_URL = process.env.BASE_URL || ''
+export const API_KEY = process.env.API_KEY || ''
+export const API_SECRET = process.env.SECRET || ''
+export const API_PASSPHRASE = process.env.PASSPHRASE || ''
+export const ACCOUNT_ID = '613215d0656e3f000614605e'
+export const FEES = 0.01 * 0.8
