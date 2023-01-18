de4c1ee1e7e924a65561d8d017ce6e5b34393a2e
Osama <Osamah.a@mblhightech.com>
2023-01-18 08:39:43 +0200

    adding more comments and fixing the logic for order size

--- src/modules/Watchdog.ts
+++ src/modules/Watchdog.ts
@@ -1,6 +1,7 @@
 import { ACCOUNTS } from 'lib/constants/account'
+import { CURRENCIES } from 'lib/constants/currencies'
 import { getHistory } from 'lib/helpers/candles'
-import { getQuote, PAIRS } from 'lib/helpers/tickers'
+import { getBase, getQuote, PAIRS } from 'lib/helpers/tickers'
 import { ITicker } from 'lib/types/tickers'
 import { IWatchdogParams } from 'lib/types/watchdog'
 import { Trader } from './Trader'
@@ -20,35 +21,65 @@ export class Watchdog {
       if (!currentPair) continue
       setTimeout(() => {
         this.outpost(currentPair)
-      }, 1000)
+      }, 1000 * i)
     }
   }
 
+  /**
+   * Runs the trading strategy on the given pair
+   * @param {ITicker} pair - The pair to run the strategy on
+   */
   outpost(pair: ITicker) {
+    // set an interval to run the trading strategy on the given pair
     setInterval(async () => {
       // get the most up to date info for that ticker
       const history = await getHistory({ symbol: pair.symbol, timeframe: this.timeframe, lookbackPeriods: 200 })
+      // get the available equity for the ticker
       const equity = ACCOUNTS[getQuote(pair.symbol)]?.available
+      // check if the history or equity is undefined
       if (!history || !equity) return
+
+      // get the current price of the ticker
       const currentPrice = history[0].close
+
       // check if the passed strategy gives a signal
       const order = this.strategy.fn({ currentPrice, history })
+      // if the strategy doesn't give a signal, return
       if (!order) return
+      // get the precision of the currency
+      const currency = CURRENCIES[getBase(pair.symbol)]
+      const precision = currency?.baseIncrement.split('.')[1].length
+      if (!precision) return
+      // calculate the order size by dividing the available equity by the current price
+      const orderSize = Number(equity) / currentPrice
+
+      // execute the trade using the trader class
       const result = await new Trader({
+        // object containing the base parameters of the order
         order: {
           baseParams: {
+            // clientOid is a unique identifier for the order
             clientOid: `${pair.symbol}_${Date.now().toString()}`,
-            side: order.side,
+            // the side of the order (buy or sell)
+            side: order?.side || 'buy',
+            // the symbol of the ticker
             symbol: pair.symbol,
+            // the type of the order (limit or market)
             type: 'limit',
           },
+          // object containing the order parameters
           orderParams: {
+            // the price at which the order should be executed
             price: currentPrice.toString(),
-            size: equity,
+            // the size of the order
+            size: orderSize.toFixed(precision - 1),
           },
         },
+        // the stop loss level for the order
         SL: order.SL,
+        // the take profit level for the order
         TP: order.TP,
+        // the name of the strategy used
         strategy: this.strategy.name,
       }).execute()
       console.log({ result })
