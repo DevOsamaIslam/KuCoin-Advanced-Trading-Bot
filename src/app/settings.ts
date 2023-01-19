e144d0f175f1b782b9c580ffa417eb17be97376c
Osama <Osamah.a@mblhightech.com>
2023-01-19 20:17:38 +0200

    add fees constant

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -6,4 +6,4 @@ export const API_KEY = process.env.API_KEY || ''
 export const API_SECRET = process.env.SECRET || ''
 export const API_PASSPHRASE = process.env.PASSPHRASE || ''
 export const ACCOUNT_ID = '613215d0656e3f000614605e'
-export const FEES = 0.08
+export const FEES = 0.15
