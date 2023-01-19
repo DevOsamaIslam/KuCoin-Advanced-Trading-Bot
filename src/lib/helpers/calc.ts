3219cff3135ad1664f615504a3f2ef1e5918c6a4
Osama <Osamah.a@mblhightech.com>
2023-01-19 20:18:18 +0200

    add helper function to round down a float

--- src/lib/helpers/calc.ts
+++ src/lib/helpers/calc.ts
@@ -4,3 +4,5 @@ export const afterFees = (value: number) => {
   const fee = (value * FEES) / 100
   return value - fee
 }
+
+export const roundDown = (number: number, decimals: number = 0) => Math.floor(number * Math.pow(10, decimals)) / Math.pow(10, decimals)
