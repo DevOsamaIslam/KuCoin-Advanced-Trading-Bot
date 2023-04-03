6dcde3239db7724302d40218c6609419576a4a70
Osama <Osamah.a@mblhightech.com>
2023-04-03 15:44:26 +0300

    separate the strategy from TPSL

--- src/modules/strategies/vwap.ts
+++ src/modules/strategies/vwap.ts
@@ -1,6 +1,7 @@
 import { ICandle } from 'lib/types/data'
-import { $strategyFn, IStrategyParams } from 'lib/types/strategy'
+import { $strategyFn } from 'lib/types/strategy'
 import { VWAP } from 'technicalindicators'
+import { previousCandles } from './TP-SL/basic'
 
 export const vwapStrategy: $strategyFn = ({ history, currentPrice }) => {
   // let ema = EMA.calculate({ period: 200, values: history.map(c => c.close) })
@@ -30,28 +31,11 @@ export const vwapStrategy: $strategyFn = ({ history, currentPrice }) => {
     lastCandle.close > vwapResult[vwapResult.length - 1] &&
     lastCandle.close - lastCandle.open > lastCandle.high - lastCandle.close // candle body is larger that its upper shadow
   if (vwapReady) {
-    const { TP, SL } = setSLandTP({ history, currentPrice })
+    const { TP, SL } = previousCandles({ history, currentPrice })
     return {
       side: 'buy',
       TP: TP.toFixed(10),
       SL: SL.toFixed(10),
     }
   }
-}
-
-function setSLandTP({ history, currentPrice }: IStrategyParams) {
-  // Find the lowest of the past 10 candles
-  const past10Candles = history.slice(-10)
-  const min = Math.min(...past10Candles.map(c => c.low))
-
-  // Set the SL at the lowest of the past 10 candles
-  const SL = min
-
-  // Calculate the percentage of the downside from the current price to the SL
-  const downside = (currentPrice - SL) / currentPrice
-
-  // Set the TP at x1.5 the percentage of the downside from the current price to the SL
-  const TP = currentPrice * (1 - 1.5 * downside)
-
-  return { SL, TP }
 }
