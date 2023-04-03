dce1086875236a5a69147d95ad2bebbae9d32b55
Osama <Osamah.a@mblhightech.com>
2023-04-03 15:41:24 +0300

    add getPriceIncrementPrecision helper function

--- src/lib/helpers/calc.ts
+++ src/lib/helpers/calc.ts
@@ -1,4 +1,5 @@
 import { FEES } from 'app/settings'
+import { ICurrency } from 'lib/types/tickers'
 
 export const afterFees = (value: number) => {
   const fee = (value * FEES) / 100
@@ -6,3 +7,10 @@ export const afterFees = (value: number) => {
 }
 
 export const roundDown = (number: number, decimals: number = 0) => Math.floor(number * Math.pow(10, decimals)) / Math.pow(10, decimals)
+
+export const getPriceIncrementPrecision = (value: number | string, currency: ICurrency) => {
+  const precision = currency.priceIncrement
+  if (typeof value === 'string') {
+    return Number(value).toFixed(+precision)
+  } else return Number(value.toFixed(+precision))
+}
